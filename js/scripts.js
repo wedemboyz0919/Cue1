1f8ff0d84faca130917d1bc6828b85b036e0a70d
saheed adewara <wedemboyz0919@gmail.com>
2021-06-26 12:13:30 -0500

    week 4

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+    $(".carousel").carousel( { interval: 5000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("toggle");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("toggle");
+    });
+});
